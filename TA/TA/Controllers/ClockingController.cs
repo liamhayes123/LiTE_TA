3fadf6a59f6c5d46e36a4400e0fc8420e9a75e07
liam_hayes123@hotmail.co.uk <liam_hayes123@hotmail.co.uk>
2016-06-12 20:25:44 +0100

    initial commit

---
+++ TA/TA/Controllers/ClockingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TA.Controllers
+{
+    public class ClockingController : Controller
+    {
+        // GET: Clocking
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TA/TA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TA.Startup))]
+namespace TA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
